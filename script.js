c09b8e243073e4b2ea859171f93ecff5b3a75ab3
kotkotenok45 <kotkotenok43434343@gmail.com>
2025-07-08 23:14:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let username = "";
 
 function connect() {
   username = document.getElementById('username').value;
-  const url = 'https://kotochat-e22r.onrender.com';
+  const url = wsDisplay.textContent.trim();
 
   ws = new WebSocket(url);
   ws.onopen = () => {
